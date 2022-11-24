5f414bad838909e1a9ef9d766d579cf8706e8e48
ABIR <hbkabir004@gmail.com>
2022-11-25 03:28:01 +0600

    CoreTeam Added

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
+import Team from './Team';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Team></Team>
         </div>
     );
 };
