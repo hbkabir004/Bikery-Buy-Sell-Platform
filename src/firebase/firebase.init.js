83f32e9083f31a925572fc68b283cddecb556c2e
ABIR <hbkabir004@gmail.com>
2022-11-25 14:50:35 +0600

    Auth updated

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCgTemtdt8_mwBx67AxCJ_V7GIwQTrnFKQ",
-  authDomain: "bookory-c83fc.firebaseapp.com",
-  projectId: "bookory-c83fc",
-  storageBucket: "bookory-c83fc.appspot.com",
-  messagingSenderId: "680031208122",
-  appId: "1:680031208122:web:0755ec097ab6b80c033883"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId:  process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
