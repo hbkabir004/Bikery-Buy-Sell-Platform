088204b6c147cee6d4a836e5862e0874346cbbba
ABIR <hbkabir004@gmail.com>
2022-11-28 18:42:53 +0600

    Dashboard Layout Added

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -3,11 +3,14 @@ import Registration from '../components/Auth/Registration';
 import SignIn from '../components/Auth/SignIn';
 import Blog from "../components/Pages/Blog";
 import BookingModal from "../components/Pages/Bookings/BookingModal";
+import AllUsers from "../components/Pages/Dashboard/AllUsers";
 import Home from "../components/Pages/Home/Home";
 import AllProducts from "../components/Pages/Products/AllProducts";
 import CategoryProducts from "../components/Pages/Products/CategoryProducts";
 import ErrorPage from "../components/Shared/ErrorPage";
+import DashboardLayout from "../layout/DashboardLayout";
 import Main from "../layout/Main";
+import AdminRoute from "./AdminRoute";
 import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
@@ -51,6 +54,24 @@ const router = createBrowserRouter([
             },
         ],
     },
+    {
+        path: '/dashboard',
+        element: <PrivateRoute><DashboardLayout></DashboardLayout></PrivateRoute>,
+        children: [
+            // {
+            //     path: '/dashboard',
+            //     element: <MyAppointment></MyAppointment>
+            // },
+            {
+                path: '/dashboard/allusers',
+                element: <AdminRoute><AllUsers></AllUsers></AdminRoute>
+            },
+            // {
+            //     path: '/dashboard/add-a-doctor',
+            //     element: <AdminRoute><AddADoctor></AddADoctor></AdminRoute>
+            // },
+        ]
+    }
 ])
 
 export default router;
---
+++ src/layout/DashboardLayout.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import NavBar from '../components/Shared/NavBar';
+import { AuthContext } from '../context/AuthProvider';
+import useAdmin from '../hooks/useAdmin';
+
+const DashboardLayout = () => {
+    const { user } = useContext(AuthContext);
+    const [isAdmin] = useAdmin(user?.email)
+    return (
+        <div>
+            <NavBar></NavBar>
+            <div className="drawer drawer-mobile">
+                <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content">
+                    <Outlet></Outlet>
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-80 bg-base-100 text-base-content font-semibold">
+                        <li><Link to='/dashboard'>My Appointments</Link></li>
+                        {
+                            isAdmin
+                            &&
+                            <>
+                                <li><Link to='/dashboard/allusers'>All Users</Link></li>
+                                <li><Link to='/dashboard/add-a-doctor'>Add a Doctor</Link></li>
+                            </>
+                        }
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DashboardLayout;
